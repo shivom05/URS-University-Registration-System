04803ec53426ac28793275e6b89ea4153c6232c6
shivom05 <s9716776214@gmail.com>
2021-07-19 17:54:07 +0530

    Update settings.py

--- plasma/settings.py
+++ plasma/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -165,3 +166,5 @@ MESSAGE_TAGS = {
     messages.ERROR: 'alert-danger',
 }
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
